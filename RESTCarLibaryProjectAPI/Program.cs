77e2d2dc2fe9ea995dc59a49c5a8259620bd6d2a
Rex77pro <smaurieka@gmail.com>
2023-02-28 22:14:03 +0100

    text not swagger

--- RESTCarLibaryProjectAPI/Program.cs
+++ RESTCarLibaryProjectAPI/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddSingleton<CarRepository>(new CarRepository());
+builder.Services.AddSingleton<CarRepository>();
 
 var app = builder.Build();
 
